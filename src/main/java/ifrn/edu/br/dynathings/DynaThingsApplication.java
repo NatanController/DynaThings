8e8414ec191db4f1670e0553d8d4ab6fd0255f01
WesleyDev01 <josewesley12040@gmail.com>
2021-02-05 20:16:15 -0300

    first commit

---
+++ src/main/java/ifrn/edu/br/dynathings/DynaThingsApplication.java
@@ -0,0 +1,13 @@
+package ifrn.edu.br.dynathings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynaThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/edu/br/dynathings/DynaThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.edu.br.dynathings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
